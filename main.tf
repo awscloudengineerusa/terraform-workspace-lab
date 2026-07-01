1181e8bc8a310f3925cb151d5917329cd59b6274
gitpractitioner <gitpractitioner@aiit.com>
2026-07-01 16:30:40 -0400

    workspace lab

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "server" {
+
+  ami           = var.ami_id
+
+  instance_type = var.instance_type
+
+  tags = {
+
+    Name = terraform.workspace
+
+  }
+
+}
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "workspace" {
+
+  value = terraform.workspace
+
+}
